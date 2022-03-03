d06bb50702347c23ddcb3e287d53499a0b4385f3
Justinas Astrauskas <justinas.astrauskas@nfq001426.foreman.nfq.lt>
2022-03-03 14:00:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [left, setLeft] = useState(0);
+  const [down, setDown] = useState(0);
+
+  const rightMove = () => {
+    left < 750 ? setLeft(left + 50) : setLeft(750);
+  };
+
+  const leftMove = () => {
+    left > 0 ? setLeft(left - 50) : setLeft(0);
+  };
+
+  const uptMove = () => {
+    down > 0 ? setDown(down - 50) : setDown(0);
+  };
+
+  const downMowe = () => {
+    down < 750 ? setDown(down + 50) : setDown(750);
+  };
+
+  console.log(left);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="mainDiv">
+        <CenterDiv>
+          <div
+            className="midleDiv"
+            style={{ transform: `translate(${left}px, ${down}px` }}
+          />
+        </CenterDiv>
+      </div>
+      <ButtonDiv>
+        <div className="div4" onClick={rightMove}>
+          RIGHT
+        </div>
+        <div className="div3" onClick={leftMove}>
+          LEFT
+        </div>
+        <div className="div1" onClick={uptMove}>
+          UP
+        </div>
+        <div className="div2" onClick={downMowe}>
+          DOWN
+        </div>
+      </ButtonDiv>
     </div>
   );
 }
 
 export default App;
+
+const CenterDiv = styled.div`
+  box-sizing: border-box;
+  width: 800px;
+  height: 800px;
+  background-color: green;
+  position: relative;
+`;
+
+const ButtonDiv = styled.div`
+  box-sizing: border-box;
+  width: 800px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+
+  div {
+    background: gray;
+    padding: 5px 0;
+    border-radius: 5px;
+    color: white;
+    text-align: center;
+    cursor: pointer;
+  }
+`;
